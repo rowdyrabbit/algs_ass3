55a0b43c0e125ac88730c26a5d7f2d161c5f7552
Denise Fernandez <denise.fernandez@iag.com.au>
2015-02-24 13:18:54 +1100

    First commit

---
+++ src/Brute.java
@@ -0,0 +1,9 @@
+
+public class Brute {
+
+    public static void main(String[] args) {
+
+
+        Point[] input = new Point[]{};
+    }
+}
---
+++ src/Point.java
@@ -0,0 +1,92 @@
+import java.util.Comparator;
+
+/*************************************************************************
+ * Name:
+ * Email:
+ *
+ * Compilation:  javac Point.java
+ * Execution:
+ * Dependencies: StdDraw.java
+ *
+ * Description: An immutable data type for points in the plane.
+ *
+ *************************************************************************/
+
+public class Point implements Comparable<Point> {
+
+    // compare points by slope
+    public final Comparator<Point> SLOPE_ORDER = new Comparator<Point>() {
+        @Override
+        public int compare(Point p1, Point p2) {
+            double p1Slope = slopeTo(p1);
+            double p2Slope = slopeTo(p2);
+            if (p1Slope < p2Slope)
+                return -1;
+            else if (p1Slope > p2Slope)
+                return 1;
+            else
+                return 0;
+        }
+    };
+
+    private final int x;                              // x coordinate
+    private final int y;                              // y coordinate
+
+    // create the point (x, y)
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    // plot this point to standard drawing
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    // draw line between this point and that point to standard drawing
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // slope between this point and that point
+    public double slopeTo(Point that) {
+        double slope = (that.y - this.y) / (that.x - that.y);
+        if (compareTo(that) == 0) {
+            return Double.NEGATIVE_INFINITY;
+        } else if (this.y == that.y) {
+            return Double.POSITIVE_INFINITY;
+        } else if (this.x == that.x) {
+            return +0;
+        } else {
+            return slope;
+        }
+    }
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        if (this.y < that.y) {
+            return -1;
+        } else if (this.y == that.y && this.x < that.x) {
+            return -1;
+        } else if (this.y == that.y && this.x == that.x) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // return string representation of this point
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
+}
